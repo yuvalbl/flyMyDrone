eea771be16db8e26e8c4f23be1c4646cde92c62a
yuvalbl <yuval.bar.levi@gmail.com>
2018-02-20 20:30:01 +0200

    basic demo

--- server.js
+++ server.js
@@ -1,56 +1,63 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const drone = require('ar-drone').createClient({ip: "192.168.1.222"});
-const MAX_SPEED = 2;
+const drone = require('ar-drone').createClient({ip: '1.1.1.222'});
+
+const SPEED = .1;
 
 // serve client files
-app.use("/client", express.static(path.join(__dirname + '/client')))
+app.use('/', express.static(path.join(__dirname + '/client')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/index.html'))
 });
 
-
-drone.on('navdata', function (navdata) {
-    io.sockets.emit("navdata", navdata)
-});
-
+// client - server socket communication
 io.on('connection', function (socket) {
     console.log('a user connected');
 
-    socket.on('keyMove', function (move) {
-        if(move.speed > MAX_SPEED) {
-            move.speed = MAX_SPEED;
+    socket.on('move', function (move) {
+        if(move === 'up' ||
+            move === 'down' ||
+            move === 'counterClockwise' ||
+            move === 'clockwise') {
+            drone[move](SPEED * 2);
+            drone.after(800, drone.stop);
+        } else {
+            drone[move](SPEED);
+            drone.after(200, drone.stop);
         }
-        console.log(`${move.direction} at speed of ${move.speed}`);
-        drone[move.direction](move.speed);
+        console.log(`${move} at speed of ${SPEED}`);
     });
 
     socket.on('takeoff', function () {
-        console.log("takeoff");
+        console.log('takeoff');
 
         drone.takeoff(function(){
-            socket.emit("takingOff");
-            console.log("airbone");
+            console.log('airbone');
         });
     });
 
     socket.on('land', function () {
-        console.log("land");
+        console.log('land');
         drone.stop();
         drone.land(function(){
-            console.log("landed");
-            socket.emit("landed")
+            console.log('landed');
         })
     })
 });
 
-
-server.listen(3000, function(){
-    console.log("Client is available at http://localhost:3000");
+app.get('/stop', function () {
+    console.log('emergency landing');
+    drone.stop();
+    drone.land(function(){
+        console.log('emergency landed');
+    })
 });
-require("dronestream").listen(server, {ip: "192.168.1.222"});
+
+
+server.listen(3001, function(){
+    console.log('Client is available at http://localhost:3001');
+});
